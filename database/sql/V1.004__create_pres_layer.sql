30cb247d6be28237bffe452052ca6efaa93418cb
mart2010 <mart2010.l@gmail.com>
2016-06-25 17:05:58 +0200

    on

--- database/sql/V1.004__create_pres_layer.sql
+++ database/sql/V1.004__create_pres_layer.sql
@@ -27,9 +27,9 @@ create table presentation.dim_site (
 -- DML given to create the table/flat file for Redshift
 insert into presentation.dim_site
 values
-('Librarything','www.librarything.com'),
-,('Goodreads','www.goodreads.com'),
-,('Babelio','www.babelio.com');
+('librarything','www.librarything.com'),
+,('goodreads','www.goodreads.com'),
+,('babelio','www.babelio.com');
 
 ---------------------------------------------------------------
 create table presentation.dim_language (
@@ -102,10 +102,60 @@ ORDER BY 1;
 
 
 ---------------------------------------------------------------
+create table presentation.dim_mds (
+    code varchar(30) primary key,
+    parent_code varchar(30),
+    original_code varchar(35),
+    value varchar(100),
+    foreign key parent_code references presentation.dim_mds(code)
+)
+--diststyle ALL
+;
 
 
-create table presentation.book (
+---------------------------------------------------------------
+create table presentation.dim_tag (
+    id varchar(??) primary key, --as lower case?
+    --etc..
+)
+--diststyle ALL
+;
+
+--colocate rel_tag with its book
+create table presentation.rel_tag (
+    tag_id varchar(??),
+    book_id bigint,
+    primary key (tag_id, book_id),
+    foreign key (tag_id) references presentation.tag(id),
+    foreign key (book_id) references presentation.book(id)
+)
+diststyle key distkey (book_id)
+;
+
+
+---------------------------------------------------------------
+create table presentation.dim_author (
     id int primary key,
+    --etc..
+)
+--diststyle ALL
+;
+
+--colocate rel_author with its book
+create table presentation.rel_author (
+    author_id varchar(??),
+    book_id bigint,
+    primary key (author_id, book_id),
+    foreign key (author_id) references presentation.author(id),
+    foreign key (book_id) references presentation.book(id)
+)
+diststyle key distkey (book_id)
+;
+
+
+---------------------------------------------------------------
+create table presentation.book (
+    id bigint primary key,
     title_ori text,
     original_lang varchar(??),
     mds_code varchar(30),
@@ -118,10 +168,11 @@ create table presentation.book (
     spanish_title varchar(500),
     italian_title varchar(500),
     japenese_title varchar(500),
-    swedish_title varchar(500),
     finish_title varchar(500),
     portuguese_title varchar(500)
-);
+)
+diststyle key distkey (id);
+;
 
 --TODO: validate this
 select coalesce(s.master_refid, w.work_refid) as id
@@ -130,21 +181,17 @@ select coalesce(s.master_refid, w.work_refid) as id
         , max(case when s.master_refid is null then w.mds_code else NULL end) as mds_code
         , max(case when s.master_refid is null then w.title else NULL end) as english_title
         , max(case when wt.lang_code = 'fre' then wt.title else NULL end) as french_title
+        , max(case when wt.lang_code = 'ger' then wt.title else NULL end) as german_title
 from integration.work_info w
 left join integration.work_sameas s on w.work_refid = s.work_refid
 left join integration.work_title wt on wt.work_refid = w.work_refid
-
-
-
 group by coalesce(s.master_refid, w.work_refid)
 
 
-
-
-
-
+---------------------------------------------------------------
 create table presentation.reviewer (
-    id int primary key,
+    id serial,
+    id_uuid uuid unique,  -- for lookup only (not exported for RS)
     username varchar(200),
     gender char(1),
     birth_year smallint,
@@ -153,67 +200,60 @@ create table presentation.reviewer (
     country varchar(100),
     region varchar(200),
     city varchar(200),
-    site_id smallint not null,
-    foreign key(site_id) references presentation.site(id)
-);
+    site_name varchar(20) not null
+)
+diststyle key distkey (id);
+
+--TODO: validate if something similar can be done
+--need to be populated prior to review (to get id)
+insert into presentation.reviewer(id_uuid, username, gender, birth_year, status, site_name)
+select u.id as id_uuid
+        , username
+        , case when random() < 0.4 then 'F' when random() < 0.6 then 'M' else 'U' end as gender
+        , cast(random() * 50 + 1950 as smallint) as birth_year
+        , s.logical_name
+from integration.user u
+join integration.site s on u.site_id = s.id
+;
 
 
-
-
-
+---------------------------------------------------------------
 create table presentation.review (
     id bigint primary key,
     id_similarto bigint,
     book_id int not null,
     reviewer_id int not null,
-    site_id smallint not null,
-    date_id smallint not null, --smart-key
+    site_name varchar(20) not null,
+    date_id date not null,
     -- all facts
     rating smallint,
     nb_likes int,
     lang_code char(3),
-    review varchar(30000),  --based on max currently found
+    -- review varchar(30000),  --based on max currently found
     foreign key (book_id) references presentation.book(id),
     foreign key (reviewer_id) references presentation.reviewer(id),
     foreign key (site_id) references presentation.site(id),
     foreign key (date_id) references presentation.dim_date(id),
-);
+)
+diststyle key distkey (book_id)
+;
 
--- Loading DML
+
 insert into presentation.review
 select r.id
         , s.other_review_id as id_similarto
-        , coalesce(ws.work_refid, w.refid) as book_id
-        , w.refid as book_id
-        , w.refid as book_id
-
+        , coalesce(ws.master_refid, r.work_refid) as book_id
+        , pr.id as reviewer_id
+        , si.logical_name as site_name
+        , r.review_date as date_id
+        , r.parsed_rating as rating
+        , r.likes as nb_likes
+        , r.review_lang as lang_id
 from integration.review r
 left join integration.review_similarto s on s.review_id = r.id
-join integration.work w on w.refid = r.work_refid
 left join integration.work_sameas ws on ws.work_refid = r.work_refid
-
-
-
-
-
---check.. if I could add this so to track more than one duplciates (the fill dupes review with master being a reference (minimum) id?)
-create table presentation.review_similarto (
-    review_id bigint,
-    master_review_id bigint,
-
-);
-
-
-create table presentation.dim_mds (
-    id int primary key
-
-);
-
---see how to best manage the many-to-many...
-create table presentation.dim_tag (
-    id int primary key
-);
-
-
+join presentation.reviewer pr on pr.id_uuid = r.user_id
+join integration.site si on si.id = r.site_id
+;
 
 
